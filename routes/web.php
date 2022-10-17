450707f5f04aee40f7f82c8a97c00e823a9eccee
Sarah Jeong <jungej0106@gmail.com>
2022-10-17 15:53:18 -0600

    feat: ep10-Use Caching for Expensive Operations

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,9 @@ Route::get('/', function () {
 Route::get('posts/{post}', function ($slug) {
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    ddd($path);
+    // ddd($path);
 
+    // if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {     // also can do this
     if (! file_exists($path)) {
         // option1 : show error message
             // ddd('file does not exist');
@@ -34,11 +35,16 @@ Route::get('posts/{post}', function ($slug) {
 
     }
 
-    $post = file_get_contents($path);
+    // cache for 20minutes
+    $post = cache()->remember("posts.{$slug}", 1200, function() use ($path) {
+        var_dump('file_get_contents');
+        return file_get_contents($path);
 
+        // $post = file_get_contents($path);
+    });
+    
     return view('post', [
         'post' => $post
-
     ]);
 }) ->where('post', '[A-z_\-]+');
 // }) ->whereAlpha('post');
