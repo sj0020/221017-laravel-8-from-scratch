0656acdabf5d46835a4f77bf561e4f16d1ee9287
Sarah Jeong <jungej0106@gmail.com>
2022-10-17 13:21:32 -0600

    feat: ep7-Make a Route and Link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">post 1</a></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum aut blanditiis quisquam iste dolor atque maxime animi? Iusto repudiandae sequi dignissimos odit veniam esse magni nisi, est quae maiores possimus.</p>
+    </article>
+
+    <a href="/">Go Back</a>
+
+</body>
+</html>
